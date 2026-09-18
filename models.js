3bebd9cfd2db175a218092584b91d8d7ffff0e8e
Zenex Admin <mdjakirhossain9744@gmail.com>
2026-09-19 01:51:21 +0600

    Changed some designs

--- models.js
+++ models.js
@@ -14,15 +14,21 @@ const userSchema = new mongoose.Schema({
     balance: Number,
     agentEarning: Number,
     autoPayEnabled: Boolean
-}, { strict: false, timestamps: true }); // 💥 Added timestamps 💥
+}, { strict: false, timestamps: true });
 
 // Order Schema
 const orderSchema = new mongoose.Schema({
     userEmail: String,
+    userName: String,         // 💥 ADDED
+    userUid: String,          // 💥 ADDED
+    agentEmail: String,       // 💥 ADDED
     searchNumber: String,
     displayNumber: String,
     country: String,
     operator: String,
+    trxId: String,            // 💥 ADDED (For Duplicate Guard)
+    requestedRange: String,   // 💥 ADDED
+    trueService: String,      // 💥 ADDED (Sender ID-এর জন্য সবচেয়ে গুরুত্বপূর্ণ)
     status: String,
     dateString: String,
     expireAt: Date,
@@ -30,7 +36,7 @@ const orderSchema = new mongoose.Schema({
     otp: String,
     orderCost: Number,
     orderCommission: Number
-}, { strict: false, timestamps: true }); // 💥 Added timestamps 💥
+}, { strict: false, timestamps: true });
 
 export const User = mongoose.model('User', userSchema);
 export const Order = mongoose.model('Order', orderSchema);
